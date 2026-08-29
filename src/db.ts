4c5e92dbb7ec27081d34875a3f00a9bf57a46eb3
Render2025 <dsolomon@protonmail.com>
2026-08-29 15:06:43 -0700

    Create db.ts

---
+++ src/db.ts
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+import type { Env } from "./types";
+
+export async function withDatabase<T>(
+  env: Env,
+  fn: (client: Client) => Promise<T>
+): Promise<T> {
+  if (!env.HYPERDRIVE?.connectionString) {
+    throw new Error("HYPERDRIVE binding is missing");
+  }
+
+  const client = new Client({
+    connectionString: env.HYPERDRIVE.connectionString
+  });
+
+  await client.connect();
+
+  try {
+    return await fn(client);
+  } finally {
+    await client.end();
+  }
+}
